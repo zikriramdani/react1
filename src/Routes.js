c0f5f09687674318c495d15c5bb8ba0dd37e7b73
ZR <zikriramdani.developer@gmail.com>
2020-09-17 12:38:48 +0700

    url

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 import Squeze from './pages/Squeze';
@@ -12,7 +12,8 @@ class Routes extends Component {
                     <Route path="/" exact component={LandingPage} />
                     <Route path="/landingpage" exact component={LandingPage} />
                     <Route path="/squeze" exact component={Squeze} />
-                    <Route component={LandingPage} />
+                    {/* <Route path="*" exact component={LandingPage} /> */}
+                    <Redirect exact from="*" to="/landingpage" />
                 </Switch>
             </BrowserRouter>
         );
