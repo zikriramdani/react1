72445c83f765c4c84b5bb4a799e439a7e8dd6286
ZR <zikriramdani.developer@gmail.com>
2020-09-22 13:42:41 +0700

    belajar

--- src/pages/LandingPage/components/SectionFourThree.jsx
+++ src/pages/LandingPage/components/SectionFourThree.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import {Container, Row, Col, Image} from 'react-bootstrap';
 
-import renderHTML from 'react-render-html'
+import renderHTML from 'react-render-html';
+
+import Yovi from '../../../assets/images/yovi.jpg';
+import Ina from '../../../assets/images/ina.jpg';
+import XiaoLian from '../../../assets/images/xiao-lian.jpg';
 
 class SectionFourThree extends Component {
     render() {
         const dataBelajar = [
-            {name: 'Mangara Sidabutar', position: 'Senior Consultant & Managing Director Madael Consult', image: 'http://placehold.it/834x450', description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
-            {name: 'Yovi', position: 'Trainer Nutrition for human with Plant Based Whole Food Diet', image: 'http://placehold.it/834x450', description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
-            {name: 'Denilson Chandra', position: 'Public Speaking, Copywriting, Training, Motivation, Sales, Leadership & Personal Branding', image: 'http://placehold.it/834x450', description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+            {name: 'Yovi', position: 'Batam', image: Yovi, description: "Terima kasih <span className='dTextWarning RobotoBold'><b>interskill</b></span>. Saya termasuk yang sulit mengerti tentang teknologi, tapi dengan <span className='dTextWarning RobotoBold'><b>interskill</b></span>, saya bisa melakukan pertemuan-pertemuan online dengan lancar dan penuh sense of security. Bagaimana bisa? Langkah-langkah yang perlu dilakukan cukup sederhana dan jelas. Setiap acara dan setiap akun memiliki proteksi sehingga tidak sembarang perangkat bisa melakukan koneksi tanpa terlebih dahulu melakukan pendaftaran acara. Saya senang dengan penanganan masalah yang cukup gesit mulai dari registrasi acara hingga acara selesai. Masalah yg sering terjadi seperti blokir web/app oleh perangkat pengguna, pengguna baru yang tidak mengerti cara daftar atau mendapat kendala saat join, bisa dengan gesit dibantu hingga sukses mengikuti acara. Saya merekomendasikan teman-teman untuk mencoba sendiri dan koleksi pengalaman pribadi menggunakan Interskill. Salam Sukses dan Bahagia."},
+            {name: 'Ina', position: 'Bogor', image: Ina, description: "Pertama kali diperkenalkan teman lalu tertarik untuk download serta join aplikasinya. Ternyataaaa, banyak banget pelatihan-pelatihan / seminar-seminar gratis buat nambah ilmu. Tidak harus datang ke lokasi, tidak perlu dandan. Dan yang penting, tidak perlu bayar buat nambah ilmu. Bahkan yang lebih keren lagi, bisa dapetin uang jajan juga kalau kita aktif ikut programnya. Sukses dan maju terus <span className='dTextWarning RobotoBold'><b>interskill</b></span>."},
+            {name: 'Xiao Lian', position: 'Batam', image: XiaoLian, description: "Suka banget dengan aplikasi <span className='dTextWarning RobotoBold'><b>INTERSKILL</b></span> yang mempermudah dan mempunyai proteksi yang sangat aman dan saat kita butuhkan sudah cepat banget ditanganin. Baru kali ini saya mencoba aplikasi ini. Pertama sih, saya pikir tidak begitu mudah aplikasinya, tapi ternyata malah sebaliknya. Mudah, jelas, tertata dan bagus sistemnya. Aku suka <span className='dTextWarning RobotoBold'><b>interskill</b></span>. Benar-benar rekomen banget aplikasi ini."}
         ]
         return (
             <Container fluid>
